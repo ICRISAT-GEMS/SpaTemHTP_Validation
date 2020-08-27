eb6a1b853303d690a908a62a866655e6fa47afc5
ISDstudents <ISDstudents@cgiar.org>
2020-08-27 14:40:38 +0530

    Complete part on: a) between experiment comparison, b) fitting of logictic (biological trend), c) plotting of the Figure 2 with biological trend
    Addition of data results about the logistic trend.

--- script/validation_script.R
+++ script/validation_script.R
@@ -24,7 +24,7 @@ setwd('F:/ICRISAT/Phenotyping/LeasyScan_article/SpaTemHTP_Validation')
 # https://github.com/ICRISAT-GEMS/SpaTemHTP
 
 
-devtools::install_github("ICRISAT-GEMS/SpaTemHTP")
+# devtools::install_github("ICRISAT-GEMS/SpaTemHTP")
 
 library(SpaTemHTP)
 
@@ -38,6 +38,7 @@ library(plyr)
 library(outliers)
 library(R.utils)
 library(dplyr)
+library(drc)
 
 
 # Cross-validation
@@ -78,6 +79,7 @@ for(i in 1:n_data){
   he_res[[i]] <- matrix(NA, nrow = n_scen, ncol = n_rep*n_fold)
 }
 
+
 ### Loop over the different dataset
 
 for(r in 1:n_data){
@@ -90,7 +92,7 @@ for(r in 1:n_data){
   
   run_ind <- 1 # indicator to fill the table results
   
-  for(i in 1:n_rep){ # cross-validation loop
+  for(i in 1:n_rep){  # cross-validation loop
     
     ### Split the observed phenotypic data into n_fold
     
@@ -187,15 +189,17 @@ for(r in 1:n_data){
           Res_pos <- which(var.mat$grp == 'Residual')
           he <- var.mat[Geno_pos, 4]/(var.mat[Geno_pos, 4]+var.mat[Res_pos, 4])
           print(paste('h2(LMM) = ', round(he, 2)))
-          he_res[data_id[i]][[1]][z, run_ind] <- he
+          he_res[data_id[r]][[1]][z, run_ind] <- he
           
           if(!is.null(pheno_pred)){
             
             cor_res <- cor(pheno_pred, data_vs_z$pheno, use = 'complete.obs')
             print(paste('cor(LMM) = ', round(cor_res, 2)))
-            pred_res[data_id[i]][[1]][z, run_ind] <- cor_res
+            pred_res[data_id[r]][[1]][z, run_ind] <- cor_res
             
           }
+          
+        }
           
           ### mixed model with spatial adjustment (SpATS)
           
@@ -212,7 +216,7 @@ for(r in 1:n_data){
             pheno_pred <- tryCatch(predict.SpATS(m_Sadj, newdata = data_vs_z),
                                    error = function(e) NULL)
             
-            he_res[data_id[i]][[1]][z+4, run_ind] <- he_Sadj <- getHeritability(m_Sadj)
+            he_res[data_id[r]][[1]][z+4, run_ind] <- he_Sadj <- getHeritability(m_Sadj)
             print(paste('h2(SpATS) = ', round(he_Sadj, 2)))
             
             if(!is.null(pheno_pred)){
@@ -220,7 +224,7 @@ for(r in 1:n_data){
               cor_res <- cor(pheno_pred$pheno, pheno_pred$predicted.values,
                              use = 'complete.obs')
               print(paste('cor(SpATS) = ', round(cor_res, 2)))
-              pred_res[data_id[i]][[1]][z+4, run_ind] <- cor_res
+              pred_res[data_id[r]][[1]][z+4, run_ind] <- cor_res
               
             } 
             
@@ -286,7 +290,7 @@ for(r in 1:n_data){
               pheno_pred <- tryCatch(predict.SpATS(m_Sadj, newdata = data_vs_z),
                                      error = function(e) NULL)
               
-              he_res[data_id[i]][[1]][z+8, run_ind] <- he_Sadj <- getHeritability(m_Sadj)
+              he_res[data_id[r]][[1]][z+8, run_ind] <- he_Sadj <- getHeritability(m_Sadj)
               print(paste('h2(SpATS) = ', round(he_Sadj, 2)))
               
               if(!is.null(pheno_pred)){
@@ -294,17 +298,17 @@ for(r in 1:n_data){
                 cor_res <- cor(pheno_pred$pheno, pheno_pred$predicted.values,
                                use = 'complete.obs')
                 print(paste('cor(SpATS) = ', round(cor_res, 2)))
-                pred_res[data_id[i]][[1]][z+8, run_ind] <- cor_res
+                pred_res[data_id[r]][[1]][z+8, run_ind] <- cor_res
                 
               } 
               
             }
             
           } # end single-step mixed model analysis (S9)
-          
-        }
         
       } # End different strategies computation
+      
+      run_ind <- run_ind + 1
       
     } # End fold loop
     
@@ -346,9 +350,9 @@ colnames(strat_tab) <- c('out_det', 'miss_imp')
 
 # a) level 1: four combination of crop and trait (CP_LA3D, CP_PH, SG_LA3D, SG_PH)
 # b) level 2: within level 1, two experiments (E1, E2)
-# c) level 3: within level 2, four strategies (S1-4)
+# c) level 3: within level 2, four strategies (S1-5)
 
-res <- vector(mode = 'list', length = dim(data_id)[1])
+res <- vector(mode = 'list', length = 4)
 names(res) <- c('CP_LA3D', 'CP_PH', 'SG_LA3D', 'SG_PH')
 
 for(i in 1:4){
@@ -358,8 +362,8 @@ for(i in 1:4){
   
   for(j in 1:2){
     
-    res[[i]][[j]] <- vector(mode = 'list', length = 4)
-    names(res[[i]][[j]]) <- paste0('S', 1:4)
+    res[[i]][[j]] <- vector(mode = 'list', length = 5)
+    names(res[[i]][[j]]) <- paste0('S', 1:5)
     
   }
   
@@ -372,7 +376,7 @@ for(i in 1:4){ # Iteration over the different combination of crop and trait
   for(j in 1:2){ # iteration over the two experiments
     
     
-    for(k in 1:4){ # iteration over the different strategies
+    for(k in 1:5){ # iteration over the different strategies
       
       load(file.path('data', paste0(data_id[i, j], '.RData')))
       
@@ -382,25 +386,36 @@ for(i in 1:4){ # Iteration over the different combination of crop and trait
       exp_des_data$row_f <- factor(exp_des_data$row)
       exp_des_data$rep <- factor(exp_des_data$rep)
       exp_des_data$block <- factor(exp_des_data$block)
-  
-      G_BLUEs <- SpaTemHTP_proc(exp_des_data,
-                                pheno_data = data[, (n_e_d_col+1):dim(data)[2]],
-                                out_det = strat_tab$out_det[k],
-                                miss_imp = strat_tab$miss_imp[k], sp_adj = TRUE,
-                                random = ~ rep +  rep:block + row_f + col_f,
-                                plot = TRUE)
       
-      res[[i]][[j]][[k]] <- G_BLUEs
-      
-      
+      if(k != 5){ # Strategies S1-S5
+        
+        G_BLUEs <- SpaTemHTP_proc(exp_des_data,
+                                  pheno_data = data[, (n_e_d_col+1):dim(data)[2]],
+                                  out_det = strat_tab$out_det[k],
+                                  miss_imp = strat_tab$miss_imp[k], sp_adj = TRUE,
+                                  random = ~ rep +  rep:block + row_f + col_f,
+                                  plot = TRUE)
+        
+        res[[i]][[j]][[k]] <- G_BLUEs
+        
+      } else { # Strategy S5
+        
+        
+        G_BLUES <- SpaTemHTP_proc(exp_des_data,
+                                    pheno_data = data[, (n_e_d_col+1):dim(data)[2]],
+                                    single_mixed_model = TRUE,
+                                    random = ~ rep +  rep:block + row_f + col_f,
+                                    plot = TRUE)
+        
+        res[[i]][[j]][[k]] <- G_BLUEs
+        
+      }
       
     }
     
   }
   
 }
-
-Sys.time()
 
 ### Save genotype BLUEs TS
 
@@ -419,20 +434,206 @@ save(res_TS, file = './results/res_TS.RData')
 
 #######
 
+# Assessment of genotype growth pattern 
+####################################### 
+
+load(file = './results/res_TS.RData')
+
+n_scen <- 5 # change later to 5 when adding S9
+
+### CP experiments
+
+CP_res <- res_TS[1:2]
+CP_res <- list(LA3D_E1 = CP_res[[1]][[1]], LA3D_E2 = CP_res[[1]][[2]],
+               PH_E1 = CP_res[[2]][[1]], PH_E2 = CP_res[[2]][[2]])
+
+res_tab_lin <- matrix(NA, n_scen, 4)
+res_tab_log <- matrix(NA, n_scen, 4)
+CP_log_curve <- vector(mode = 'list', length = 4)
+names(CP_log_curve) <- c('LA3D_E1', 'LA3D_E2', 'PH_E1', 'PH_E2')
+for(i in 1:4){CP_log_curve[[i]] <- vector(mode = 'list', length = n_scen)
+names(CP_log_curve[[i]]) <- paste0('S', 1:n_scen)}
+
+for(i in 1:4){
+  
+  G_TS <- CP_res[[i]]
+  
+  for(j in 1:n_scen){
+    
+    trend_j <- TS_trend(data = G_TS[[j]])
+    
+    res_tab_lin[j, i] <- round(mean(trend_j$lin_R2, na.rm = TRUE), 2)
+    res_tab_log[j, i] <- round(mean(trend_j$log_R2, na.rm = TRUE), 2)
+    CP_log_curve[[i]][[j]] <- trend_j$log_val
+    
+  }
+  
+}
+
+save(CP_log_curve, file = './results/CP_log_curve.RData')
+
+### SG experiments
+
+SG_res <- res_TS[3:4]
+SG_res <- list(LA3D_E1 = SG_res[[1]][[1]], LA3D_E2 = SG_res[[1]][[2]],
+               PH_E1 = SG_res[[2]][[1]], PH_E2 = SG_res[[2]][[2]])
+
+res_tab_lin <- matrix(NA, n_scen, 4)
+res_tab_log <- matrix(NA, n_scen, 4)
+SG_log_curve <- vector(mode = 'list', length = 4)
+names(SG_log_curve) <- c('LA3D_E1', 'LA3D_E2', 'PH_E1', 'PH_E2')
+for(i in 1:4){SG_log_curve[[i]] <- vector(mode = 'list', length = n_scen)
+names(SG_log_curve[[i]]) <- paste0('S', 1:n_scen)}
+
+for(i in 1:4){
+  
+  G_TS <- SG_res[[i]]
+  
+  for(j in 1:n_scen){
+    
+    trend_j <- TS_trend(data = G_TS[[j]])
+    
+    res_tab_lin[j, i] <- round(mean(trend_j$lin_R2, na.rm = TRUE), 2)
+    res_tab_log[j, i] <- round(mean(trend_j$log_R2, na.rm = TRUE), 2)
+    SG_log_curve[[i]][[j]] <- trend_j$log_val
+    
+  }
+  
+}
+
+save(SG_log_curve, file = './results/SG_log_curve.RData')
+
+###########
+
 # Plots of the genotype BLUEs TS (Figure 2)
 ###########################################
 
-# Vincent does that. In any case, there is already a ploting option integrate
-# in the package function SpaTemHTP_proc
+load(file = './results/res_TS.RData')
+
+### CPE2 LA3D
+
+# raw data
+
+load(file.path('data', "CP_E2_LA3D.RData"))
+
+geno_id <- data$genotype
+n_days <- dim(data)[2] - 5
+n_geno <- 288
+
+g_means <- matrix(NA, n_geno, n_days)
+
+for(i in 1:dim(g_means)[2]){
+  
+  g_av <- tapply(X = data[, i + 5], INDEX = geno_id,
+                 FUN = function(x) mean(x, na.rm = TRUE))
+  g_means[, i] <- g_av
+  
+}
+
+# calculate the biological trend (logistic curve) on the raw data
+
+trend_raw <- TS_trend(g_means)
+dt_trend <- colMeans(x = trend_raw$log_val, na.rm = TRUE)
+dt_trend <- data.frame(trait = dt_trend, day = 1:n_days, geno = 'G_av')
+
+dt <- data.frame(geno = rep(names(g_av), n_days), day = rep(1:n_days, each = n_geno),
+                 trait = c(g_means))
+
+ggplot(data = dt, aes(x = day, y = trait, group = geno)) +
+  geom_line(aes(group = geno)) + geom_line(data = dt_trend, color = 'red', size = 1) +
+  ggtitle('CPE2-LA3D raw data')
+
+
+# Strategies (S5-9)
+
+load(file = './results/CP_log_curve.RData')
+log_curve <- CP_log_curve$LA3D_E2
+
+data_sc <- res_TS[[1]][[2]]
+title_id <- paste('CPE2-LA3D', paste0('S', 5:9))
+
+for(i in 1:5){
+  
+  data_i <- data_sc[[i]]
+  n_days <- dim(data_i)[2]
+  
+  dt <- data.frame(geno = rep(rownames(data_i), n_days),
+                   day = rep(1:n_days, each = dim(data_i)[1]),
+                   trait = c(data_i))
+  
+  dt_trend <- colMeans(x = log_curve[[i]], na.rm = TRUE)
+  dt_trend <- data.frame(trait = dt_trend, day = 1:n_days, geno = 'G_av')
+  
+  plot <- ggplot(data = dt, aes(x = day, y = trait, group = geno)) +
+    geom_line(aes(group = geno)) + geom_line(data = dt_trend, color = 'red', size = 1) +
+    ggtitle(title_id[i])
+  print(plot)
+  
+}
+
+
+### SGE2 PH
+
+# raw data
+
+load(file.path('data', "SG_E2_PH.RData"))
+
+geno_id <- data$genotype
+n_days <- dim(data)[2] - 5
+n_geno <- 384
+
+g_means <- matrix(NA, n_geno, n_days)
+
+for(i in 1:dim(g_means)[2]){
+  
+  g_av <- tapply(X = data[, i + 5], INDEX = geno_id,
+                         FUN = function(x) mean(x, na.rm = TRUE))
+  g_means[, i] <- g_av
+  
+}
+
+# calculate the biological trend (logistic curve) on the raw data
+
+trend_raw <- TS_trend(g_means)
+dt_trend <- colMeans(x = trend_raw$log_val, na.rm = TRUE)
+dt_trend <- data.frame(trait = dt_trend, day = 1:n_days, geno = 'G_av')
+
+dt <- data.frame(geno = rep(names(g_av), n_days), day = rep(1:n_days, each = n_geno),
+                 trait = c(g_means))
+
+ggplot(data = dt, aes(x = day, y = trait, group = geno)) +
+  geom_line(aes(group = geno)) + geom_line(data = dt_trend, color = 'red', size = 1) +
+  ggtitle('SGE2-PH raw data')
+
+# Strategies (S5-9)
+
+load(file = './results/SG_log_curve.RData')
+log_curve <- SG_log_curve$PH_E2
+
+data_sc <- res_TS[[4]][[2]]
+title_id <- paste('SG-PH', paste0('S', 5:9))
+
+for(i in 1:5){
+  
+  data_i <- data_sc[[i]]
+  n_days <- dim(data_i)[2]
+  
+  dt <- data.frame(geno = rep(rownames(data_i), n_days),
+                   day = rep(1:n_days, each = dim(data_i)[1]),
+                   trait = c(data_i))
+  
+  dt_trend <- colMeans(x = log_curve[[i]], na.rm = TRUE)
+  dt_trend <- data.frame(trait = dt_trend, day = 1:n_days, geno = 'G_av')
+  
+  plot <- ggplot(data = dt, aes(x = day, y = trait, group = geno)) +
+    geom_line(aes(group = geno)) + geom_line(data = dt_trend, color = 'red', size = 1) +
+    ggtitle(title_id[i])
+  print(plot)
+  
+}
 
 ########
 
-# Assessment of genotyp growth pattern (linear trend fit)
-#########################################################
-
-### Soumya
-
-########
 
 # Clustering
 ############
